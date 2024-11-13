265bd1589469bfa2af977d2cc99e4d36eba228b6
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2024-11-13 23:40:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove duplicates in array/remove-duplicates-in-array.py
@@ -0,0 +1,30 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+class Solution:
+    def removeDuplicates(self, arr):
+        # code here 
+        ar=[]
+        for i in arr:
+            if i not in ar:
+                ar.append(i)
+        return ar
+
+#{ 
+ # Driver Code Starts.
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    while t > 0:
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        ans = ob.removeDuplicates(arr)
+        print(*ans)
+        print("~")
+        t -= 1
+
+
+# } Driver Code Ends
