57098b10b60beb8c692689e3c299db428fa3bc2f
Nandipati Ayyappa Reddy <119839645+ayyappareddynandipati@users.noreply.github.com>
2024-12-25 11:39:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/While loop- printTable - Java/while-loop-print-table-java.java
@@ -0,0 +1,22 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+//Back-end complete function Template for Java
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        // Your Code Here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int num = 10;
+        while(num>0)
+        {
+            System.out.print(num*n+" ");
+            num--;
+        }
+    }
+}
+
+//{ Driver Code Starts.
+// } Driver Code Ends
