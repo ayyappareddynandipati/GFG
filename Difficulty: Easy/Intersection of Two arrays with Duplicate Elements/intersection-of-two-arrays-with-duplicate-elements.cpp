e4352e5753480f8e6d9312bfdfe18e71ac7fdc3d
Nandipati Ayyappa Reddy <119839645+ayyappareddynandipati@users.noreply.github.com>
2024-12-29 08:50:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Intersection of Two arrays with Duplicate Elements/intersection-of-two-arrays-with-duplicate-elements.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> intersectionWithDuplicates(vector<int>& a, vector<int>& b) {
+        // code here
+        set<int> res1(a.begin(),a.end());
+        set<int> res2(b.begin(),b.end());
+        unordered_map<int,int> freq;
+        for(int i : res1)
+        {
+            freq[i]++;
+        }
+        for(int i : res2)
+        {
+            freq[i]++;
+        }
+        vector<int> fres;
+        for(auto num : freq)
+        {
+            if (num.second > 1) 
+            {
+                fres.push_back(num.first);
+            }
+        }
+        return fres;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr1, arr2;
+        string input;
+
+        // Read first array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr1.push_back(number);
+        }
+
+        // Read second array
+        getline(cin, input);
+        stringstream ss2(input);
+        while (ss2 >> number) {
+            arr2.push_back(number);
+        }
+
+        Solution ob;
+        vector<int> res = ob.intersectionWithDuplicates(arr1, arr2);
+        sort(res.begin(), res.end());
+
+        if (res.size() == 0) {
+            cout << "[]" << endl;
+        } else {
+            for (auto it : res)
+                cout << it << " ";
+            cout << endl;
+        }
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
