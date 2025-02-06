1ca8851e77a3a87ac4e21af9c3525e9eb3bda572
Nandipati Ayyappa Reddy <119839645+ayyappa4512@users.noreply.github.com>
2025-02-06 17:42:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Total Digits in a Number/count-total-digits-in-a-number.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    // Complete this function
+    int countDigits(int n) {
+        // Your code here
+        if(n<10) return 1;
+        else return countDigits(n/10)+1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+
+    // taking testcases
+    cin >> T;
+    while (T--) {
+        int n;
+
+        // taking number n
+        cin >> n;
+
+        // calling countDigits
+        Solution obj;
+        cout << obj.countDigits(n) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
