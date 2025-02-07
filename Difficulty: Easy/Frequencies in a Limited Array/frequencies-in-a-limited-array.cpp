2606f7db48993640c63f9ce2569145440f61018e
Nandipati Ayyappa Reddy <119839645+ayyappa4512@users.noreply.github.com>
2025-02-07 23:02:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Frequencies in a Limited Array/frequencies-in-a-limited-array.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to count the frequency of all elements from 1 to N in the array.
+    vector<int> frequencyCount(vector<int>& arr) {
+        // code here
+        int n = arr.size();         
+        map<int, int> mpp;           
+        vector<int> res(n, 0);  
+        
+        for(int i = 0; i < n; i++) {             
+            mpp[arr[i]]++;             
+        }
+        
+
+        for(int i = 0; i < n; i++) {             
+            
+            if(mpp.find(i + 1) != mpp.end()) {
+                res[i] = mpp[i + 1];
+            } else {
+                res[i] = 0;
+            }
+        }
+        
+        return res; 
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
+    while (t--) {
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num); // Read the array elements from input string
+
+        Solution obj;
+        vector<int> result = obj.frequencyCount(a);
+
+        // Print the result in the required format
+        if (result.empty()) {
+            cout << "[]"; // Print empty brackets if no duplicates are found
+        } else {
+            for (int i = 0; i < result.size(); i++) {
+                if (i != 0)
+                    cout << " ";
+                cout << result[i];
+            }
+        }
+        cout << endl; // Ensure new line after each test case output
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
