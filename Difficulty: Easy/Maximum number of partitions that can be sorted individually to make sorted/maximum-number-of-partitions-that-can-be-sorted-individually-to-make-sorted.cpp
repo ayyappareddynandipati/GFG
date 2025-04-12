6b46651692575aa5aa2220dcf2260c47a766df44
Nandipati Ayyappa Reddy <119839645+ayyappareddynandipati@users.noreply.github.com>
2025-04-12 20:29:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Maximum number of partitions that can be sorted individually to make sorted/maximum-number-of-partitions-that-can-be-sorted-individually-to-make-sorted.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int maxPartitions(vector<int>& arr) {
+        // code here
+        int cnt=0,max_seen=0;
+        for(int i=0;i<arr.size();i++){
+            max_seen = max(max_seen,arr[i]);
+            if(max_seen==i) cnt++;
+        }
+        return cnt;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.maxPartitions(arr);
+        cout << res << endl;
+        cout << "~" << endl;
+        // string tl;
+        // getline(cin, tl);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
