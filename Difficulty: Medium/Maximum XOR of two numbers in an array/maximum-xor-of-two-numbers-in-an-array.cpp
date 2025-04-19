4bbdbe3d55da6d3f8417e09e6269c24fb106998e
Nandipati Ayyappa Reddy <119839645+ayyappareddynandipati@users.noreply.github.com>
2025-04-19 07:28:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum XOR of two numbers in an array/maximum-xor-of-two-numbers-in-an-array.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+
+class Solution {
+  public:
+    int maxXor(vector<int> &arr) {
+        // code here
+        int maxXor = 0;
+        int mask = 0;
+    
+        for (int i = 31; i >= 0; --i) {
+            mask |= (1 << i);
+            unordered_set<int> prefixes;
+    
+            // Store all prefixes with current mask
+            for (int num : arr) {
+                prefixes.insert(num & mask);
+            }
+    
+            int candidate = maxXor | (1 << i); // Try to set this bit
+    
+            for (int prefix : prefixes) {
+                if (prefixes.count(prefix ^ candidate)) {
+                    maxXor = candidate; // Found two prefixes that give candidate
+                    break;
+                }
+            }
+        }
+    
+        return maxXor;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+
+        Solution ob;
+        cout << ob.maxXor(arr) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+}
+
+// } Driver Code Ends
