0557db448f761fff1b157a3b4000e2d518e45ff8
Nandipati Ayyappa Reddy <119839645+ayyappareddynandipati@users.noreply.github.com>
2025-05-04 08:17:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/nCr/n-cr.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+
+class Solution {
+  public:
+    int nCr(int n, int r) {
+        // code here
+        if (r > n) return 0;
+    
+        // Initialize a 1D array to store results of subproblems
+        int C[r + 1] = {0};
+        C[0] = 1; // Base case: nC0 = 1
+    
+        // Calculate value of Binomial Coefficient in bottom up manner
+        for (int i = 1; i <= n; i++) {
+            // Compute next row of Pascal's triangle using previous row
+            for (int j = min(i, r); j > 0; j--)
+                C[j] = C[j] + C[j - 1];
+        }
+    
+        return C[r];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, r;
+        cin >> n >> r;
+
+        Solution ob;
+        cout << ob.nCr(n, r) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
