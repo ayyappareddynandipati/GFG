013ac4ce055038bc61c25633f1a7186dc67d23c4
Nandipati Ayyappa Reddy <119839645+ayyappareddynandipati@users.noreply.github.com>
2025-06-03 21:31:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Substrings with K Distinct/substrings-with-k-distinct.cpp
+++ Difficulty: Medium/Substrings with K Distinct/substrings-with-k-distinct.cpp
@@ -1,54 +1,27 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-// User function template for C++
-
 class Solution {
   public:
-    int substringsWithAtLeastKDistinct(string s, int n, int k)
-    {
-        int l=0,res=0;
-        unordered_map<char,int>mpp;
-        for(int r=0;r<n;r++){
-            mpp[s[r]]++;
-            while(mpp.size()>=k){
-                mpp[s[l]]--;
-                if(mpp[s[l]]==0) mpp.erase(s[l]);
+    int atMostKDistinct(string& s, int k) {
+        int l = 0, res = 0;
+        unordered_map<char, int> freq;
+
+        for (int r = 0; r < s.size(); r++) {
+            freq[s[r]]++;
+
+            while (freq.size() > k) {
+                freq[s[l]]--;
+                if (freq[s[l]] == 0)
+                    freq.erase(s[l]);
                 l++;
             }
-            res+=(r-l+1);
+
+            res += (r - l + 1);
         }
+
         return res;
     }
+    
     int countSubstr(string& s, int k) {
         // code here.
-    	int n=s.size();
-    	return substringsWithAtLeastKDistinct(s, n, k+1)-substringsWithAtLeastKDistinct(s, n, k);
+        return atMostKDistinct(s, k) - atMostKDistinct(s, k - 1);
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string s;
-        cin >> s;
-        int k;
-        cin >> k;
-        Solution ob;
-        cout << ob.countSubstr(s, k) << endl;
-
-        cout << "~"
-             << "\n";
-    }
-}
-// } Driver Code Ends
