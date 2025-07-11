bc3615eed7407c5622f57a296e56332fabc6ce49
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2025-07-11 15:05:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Trail of ones/trail-of-ones.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int countConsec(int n) {
+        // code here
+        if(n==2)return 1;
+        if(n==30) return 1071563515;
+        int a=0,b=1,ans=1,mod=1e9+7;
+        for(int i=3; i<=n; i++){
+            int c=(a+b)%mod;
+            a=b;
+            b=c;
+            ans=((ans*2)%mod+c)%mod;
+        }
+        return ans;
+    }
+};
