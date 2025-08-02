eae43890a27164f7d22cdf03386d2d3de6323704
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2025-08-02 10:05:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Xoring and Clearing - Java/xoring-and-clearing-java.java
@@ -0,0 +1,26 @@
+class Geeks {
+
+    static void printArr(int arr[], int n) {
+        // Your code for printing arr here
+
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    static void setToZero(int arr[], int n) {
+        // use Arrays.fill to set arr to zero
+        for(int i=0;i<n;i++){
+            arr[i]=0;
+        }
+    }
+
+    static void xor1ToN(int arr[], int n) {
+
+        // Xor arr[i]^i
+        for(int i=0;i<n;i++){
+            arr[i]^=i;
+        }
+    }
+}
